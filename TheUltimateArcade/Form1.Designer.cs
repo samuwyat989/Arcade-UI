8ea5e012fb296ee6f8990a23861c612a9baa9715
samuwyat989 <samuwyat989@ed.amdsb.ca>
2016-10-17 17:25:00 -0700

    Add project files.

---
+++ TheUltimateArcade/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class theUltimateArcade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(theUltimateArcade));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApplication1.Properties.Resources.pacman;
+            this.pictureBox3.Location = new System.Drawing.Point(530, 145);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(154, 131);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApplication1.Properties.Resources.Pong_Screen;
+            this.pictureBox2.Location = new System.Drawing.Point(293, 145);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 131);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.snake;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 145);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // theUltimateArcade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.plastic_texture;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(746, 484);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "theUltimateArcade";
+            this.Text = "The Ultimate Arcade";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ TheUltimateArcade/Form1.cs
@@ -0,0 +1,84 @@
+﻿/// Designed By: Sam Wyatt
+/// October 19 2016
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class theUltimateArcade : Form
+    {
+        public theUltimateArcade()
+        {
+            InitializeComponent();
+            // Initialize the user-defined button, 
+            // including defining handler for Click message, 
+            // location and size.
+
+            //snake game button
+            myButtonObject myButton1 = new myButtonObject();
+            EventHandler myHandler = new EventHandler(myButton_Click_1);
+            myButton1.Click += myHandler;
+            myButton1.Location = new System.Drawing.Point(80, 340);
+            myButton1.Size = new System.Drawing.Size(101, 101);
+            this.Controls.Add(myButton1);
+
+            //pong game button
+            myButtonObject myButton2 = new myButtonObject();
+            EventHandler myHandler2 = new EventHandler(myButton2_Click);
+            myButton2.Click += myHandler2;
+            myButton2.Location = new System.Drawing.Point(280, 340);
+            myButton2.Size = new System.Drawing.Size(101, 101);
+            this.Controls.Add(myButton2);
+
+            //pac man game button
+            myButtonObject myButton3 = new myButtonObject();
+            EventHandler myHandler3 = new EventHandler(myButton3_Click);
+            myButton3.Click += myHandler3;
+            myButton3.Location = new System.Drawing.Point(480, 340);
+            myButton3.Size = new System.Drawing.Size(101, 101);
+            this.Controls.Add(myButton3);
+        }
+
+        public class myButtonObject : UserControl
+        {
+            // Draw the buttons. 
+            protected override void OnPaint(PaintEventArgs e)
+            {
+                Graphics graphics = e.Graphics;
+                Pen myPen = new Pen(Color.Silver, 4);
+                SolidBrush myBrush = new SolidBrush(Color.FromArgb(205, 0, 0));
+                SolidBrush myBrush2 = new SolidBrush(Color.Red);
+
+                Point[] shape = { new Point(10, 10), new Point(10, 0), new Point(90, 0), new Point(90, 10), new Point(100, 10),
+                new Point(100,90), new Point(90,90), new Point(90, 100), new Point(10, 100), new Point(10,90), new Point(0, 90),
+                new Point(0,10)};
+                graphics.FillPolygon(myBrush, shape);
+                graphics.DrawPolygon(myPen, shape);
+                myPen.Dispose();
+                myBrush.Dispose();
+            }
+        }
+        
+        private void myButton_Click_1(object sender, EventArgs e)
+        {
+            Process.Start("Snake Game.exe");
+        }
+
+        private void myButton2_Click(object sender, EventArgs e)
+        {
+            Process.Start("Pong.exe");
+        }
+
+        private void myButton3_Click(object sender, EventArgs e)
+        {
+            Process.Start("AnimatedGameSummative.exe");
+        }
+    }
+}
